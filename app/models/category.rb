fae95d66dde8b67597b09d5f0a5448fdb23339dd
Michael Stephens <mdstephens26@yahoo.com>
2021-09-19 19:38:22 -0400

    add validate choices length

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
     has_many :choices
 
     validates :title, presence: true
+    validates :choices, length: {minimum: 2, maximum: 2}
 
     accepts_nested_attributes_for :choices
 end
--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,4 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :title
+  has_many :choices
 end
