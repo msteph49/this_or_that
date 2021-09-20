5809dbf3a15c3d06beb6a18f9303d06a90b61093
Michael Stephens <mdstephens26@yahoo.com>
2021-09-19 22:23:40 -0400

    add popularity meth to choice model

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,9 +1,19 @@
 class Choice < ApplicationRecord
   belongs_to :category
 
-  has_many :votes
+  has_many :votes, dependent: :destroy
 
   validates :title, presence: true
-  
+  validates :number_of_choices
+
+  def popularity
+    votes.count
+  end
+
+  private
+    def number_of_choices
+      return unless self.class.where(category: category).count >= 2
+      errors.add :category, "cannot exceed 2 choices"
+    end 
   
 end
