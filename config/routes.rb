736419bfef2ce42e62d678bf90b6a0a241f8d219
Michael Stephens <mdstephens26@yahoo.com>
2021-09-26 18:22:44 -0400

    added vote route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :choices
-  resources :categories
+  resources :categories do
+    member do
+      post :vote
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
